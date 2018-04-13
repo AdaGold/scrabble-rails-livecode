80267da6a69daedce897e0cec57031e5df99c9eb
Charles Ellis <hamled@hamled.name>
2018-04-12 20:43:30 -0700

    Player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410174605) do
+ActiveRecord::Schema.define(version: 20180413033529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "tile_tray", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "turns", force: :cascade do |t|
     t.string "word"
