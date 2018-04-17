4be6152e2357f22a43cbb6af035e84a73ba57570
Charles Ellis <hamled@hamled.name>
2018-04-17 16:54:24 -0700

    Player details page: can play words

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,11 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
+
+  def play
+    @player = Player.find(params[:player_id])
+    @player.play!(params[:word])
+
+    redirect_to @player
+  end
 end
