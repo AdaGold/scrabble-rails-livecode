b8657bb3e50941ae684b5cdfefad51c23a6eaa44
Charles Ellis <hamled@hamled.name>
2018-04-17 19:10:46 -0700

    Display error message when playing invalid word

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,9 @@ class PlayersController < ApplicationController
   def play
     @player = Player.find(params[:player_id])
     @player.play!(params[:word])
-
+  rescue ArgumentError => e
+    flash[:alert] = "Could not play word: #{e.message}"
+  ensure
     redirect_to @player
   end
 end
